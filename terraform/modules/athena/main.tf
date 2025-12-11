8244367bfcad88a5bf30d70b80a8c520d8863373
Mike Marcotte <mike.marcotte@gmail.com>
2025-12-11 14:13:33 -0500

    fix terraform errors

--- terraform/modules/athena/main.tf
+++ terraform/modules/athena/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "athena_database_bucket" {
   bucket = "${var.project_name}-${var.environment}-athena-database"
 }
 
-resource "aws_s3_bucket_acl" "athena_results_bucket" {
+resource "aws_s3_bucket" "athena_results_bucket" {
   bucket = "${var.project_name}-${var.environment}-athena-results"
 }
 
--- terraform/modules/sqs/main.tf
+++ terraform/modules/sqs/main.tf
@@ -12,15 +12,15 @@ resource "aws_sqs_queue" "job_queue" {
   name                       = local.queue_name
   visibility_timeout_seconds = 1800
   tags                       = local.tags
-  redrive_policy = {
+  redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.dead_letter_queue.arn
     maxReceiveCount     = 5
-  }
+  })
 }
 
 resource "aws_sqs_queue" "dead_letter_queue" {
   name                       = "${local.queue_name}-dlq"
   visibility_timeout_seconds = 1800
-  message_retention_seconds = 1209600
+  message_retention_seconds  = 1209600
   tags                       = local.tags
 }
