cf41b4f0cbddce68af72620944aab4e9b991bdf3
Mike Marcotte <mike.marcotte@gmail.com>
2025-12-15 22:15:45 -0500

    change integration method for api gateway

--- terraform/modules/api-gateway/main.tf
+++ terraform/modules/api-gateway/main.tf
@@ -12,14 +12,14 @@ resource "aws_api_gateway_rest_api" "rest" {
   tags = var.common_tags
 }
 
-#GET /get-report?year={year}&month={month}
+# GET /get-report?year={year}&month={month}
 resource "aws_api_gateway_resource" "get_report" {
   rest_api_id = aws_api_gateway_rest_api.rest.id
   parent_id   = aws_api_gateway_rest_api.rest.root_resource_id
   path_part   = "get-report"
 }
 
-#Methods
+# Method
 resource "aws_api_gateway_method" "get_report_get" {
   rest_api_id   = aws_api_gateway_rest_api.rest.id
   resource_id   = aws_api_gateway_resource.get_report.id
@@ -33,8 +33,8 @@ resource "aws_api_gateway_integration" "get_report_integration" {
   resource_id          = aws_api_gateway_resource.get_report.id
   http_method          = aws_api_gateway_method.get_report_get.http_method
   type                 = "AWS_PROXY"
-  integration_http_method = "GET"
-  uri         = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${var.lambda_function_arns["getReport"]}/invocations"
+  integration_http_method = "POST"
+  uri                  = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${var.lambda_function_arns["getReport"]}/invocations"
 }
 
 #Deployment 
